43e24f2806b787f31487bde3cb7b096548e0b42a
taodongl <taodongl@outlook.com>
2023-05-02 21:07:45 +0800

    First Commit

---
+++ StopWatchComposeDesktop/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.6.4")
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "StopWatchComposeDesktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}
